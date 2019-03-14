1d37bd63c70da11c004aaae99472eb4d2129d9ea
kamex737 <kamex737@gmail.com>
2019-03-14 19:32:21 +0100

    Added main class and gradle dependencies

---
+++ src/main/java/com/github/kbednarz/Main.java
@@ -0,0 +1,38 @@
+package com.github.kbednarz;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.eclipse.jetty.servlet.ServletContextHandler.NO_SESSIONS;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        Server server = new Server(8080);
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(NO_SESSIONS);
+
+        servletContextHandler.setContextPath("/");
+        server.setHandler(servletContextHandler);
+
+        ServletHolder servletHolder = new ServletHolder(new ServletContainer());
+        servletContextHandler.addServlet(servletHolder, "/api/*");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            logger.error("Error occurred while starting Jetty", ex);
+            System.exit(1);
+        } finally {
+            server.destroy();
+        }
+    }
+
+}

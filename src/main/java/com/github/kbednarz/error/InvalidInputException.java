94f1e118426d426ff37d44d65ac08554e762fdb7
kamex737 <kamex737@gmail.com>
2019-03-15 20:09:04 +0100

    impl getting account from db

---
+++ src/main/java/com/github/kbednarz/error/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.github.kbednarz.error;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/github/kbednarz/service/Datastore.java
+++ src/main/java/com/github/kbednarz/service/Datastore.java
@@ -1,9 +1,14 @@
 package com.github.kbednarz.service;
 
+import com.github.kbednarz.domain.Account;
+import com.github.kbednarz.error.InvalidInputException;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
 
 public class Datastore {
+    private final static String ACCOUNT_COLLECTION = "accounts";
     private final DB db;
 
     public Datastore() {
@@ -11,5 +16,17 @@ public class Datastore {
                 .memoryDB()
                 .closeOnJvmShutdown()
                 .make();
+    }
+
+    public Account getAccount(String accountNumber) throws InvalidInputException {
+        Account account = getAccountCollection().get(accountNumber);
+        if (account == null)
+            throw new InvalidInputException("Account with number: " + accountNumber + " does not exist");
+
+        return account;
+    }
+
+    private HTreeMap<String, Account> getAccountCollection() {
+        return db.hashMap(ACCOUNT_COLLECTION, Serializer.STRING, Serializer.JAVA).createOrOpen();
     }
 }

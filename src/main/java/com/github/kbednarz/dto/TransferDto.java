277ad88afa5ff9ec1574102222f97f882c12c4d5
kamex737 <kamex737@gmail.com>
2019-03-18 20:24:11 +0100

    added transferdto

--- src/main/java/com/github/kbednarz/dto/TransferDto.java
+++ src/main/java/com/github/kbednarz/dto/TransferDto.java
@@ -4,4 +4,13 @@ public class TransferDto {
     public String from;
     public String to;
     public long amount;
+
+    public TransferDto() {
+    }
+
+    public TransferDto(String from, String to, long amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
 }
